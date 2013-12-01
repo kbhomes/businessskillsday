92eddaed23970ab90dde17bc2ba6d74931b50f90
Sajid Anwar <sajidanwar94@gmail.com>
2013-12-01 15:43:28 -0700

    Change nametags format

--- app/reports/nametags_report.rb
+++ app/reports/nametags_report.rb
@@ -4,10 +4,10 @@ class NametagsReport
   def initialize(students)
     @students = students
     @pdf = Prawn::Document.new(
-        :left_margin => (0.75).in,
-        :right_margin => (0.75).in,
-        :top_margin => 1.in,
-        :bottom_margin => 1.in,
+        :left_margin => (0.7).in,
+        :right_margin => (0.7).in,
+        :top_margin => (0.56).in,
+        :bottom_margin => (0.5).in,
     )
     @pdf.font_size 10
     @pdf.stroke_color '888888'
@@ -15,12 +15,15 @@ class NametagsReport
 
     @students = @students.order{chapter.name}.references(:chapter)
 
-    width = (3.5).in
-    height = (2.22).in
+    width = (3.38).in
+    height = (2.33).in
     margin = (0.1).in
 
+    horizontal_gap = (0.34).in
+    vertical_gap = (0.21).in
+
     @students.each_with_index do |student, i|
-      left = (i % 2 == 0) ? 0 : width
+      left = (i % 2 == 0) ? 0 : (width + horizontal_gap)
       top = @pdf.cursor
 
       name = "#{student.name}\n"
@@ -28,7 +31,7 @@ class NametagsReport
       events = student.events.map { |event| "#{event.number.to_s.rjust(3, '0')} - #{event.to_s}" }.join("\n")
 
       @pdf.bounding_box([left, top], :width => width, :height => height) do
-        @pdf.stroke_bounds
+        # @pdf.stroke_bounds
         @pdf.bounding_box([margin, height - margin], :width => width - 2*margin, :height => height - 2*margin) do
           @pdf.formatted_text(
               [
@@ -47,7 +50,13 @@ class NametagsReport
 
       # If we've finished the first student in the row,
       # then move back up so we can finish the second student
-      @pdf.move_up height if (i % 2 == 0)
+      # Else, move down by the gap.
+      if (i % 2 == 0)
+        @pdf.move_up height
+      else
+        @pdf.move_down vertical_gap
+      end
+
 
       # Start a new page if we've just finished the 8th student for this page.
       @pdf.start_new_page if (i % 8 == 7)
